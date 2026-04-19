a9c78474fb1f1ca32f52cfddc067c4d20009ce49
Bennuri Akshaya <akshayabennuri@gmail.com>
2026-04-19 13:19:33 +0530

    fix redis connection to upstash

--- backend/execution/queue.js
+++ backend/execution/queue.js
@@ -1,29 +1,26 @@
-//Export the queue instanec and the redis connection so both the producer(adding jobs) and worker(processing jobs) share the same connection
+// 
 
 const { Queue } = require("bullmq");
-const { Redis } = require("ioredis");
+const IORedis = require("ioredis");
 const dotenv = require('dotenv');
+dotenv.config();
 
-//Shared redis connection config
-//BullMq requires maxRetriesPerRequest:null for blocking operations
-const redisConfig = {
-    host:"127.0.0.1",
-    port: 6379,
-    maxRetriesPerRequest : null, //required by BullMQ
-};
+// Use Upstash Redis URL
+const queueConnection = new IORedis(process.env.REDIS_URL, {
+    maxRetriesPerRequest: null
+});
 
-//Seperate connection instances BullMQ requires seperate connections for Queue and Worker
-const queueConnection = new Redis(redisConfig);
-const workerConnection = new Redis(redisConfig);
+const workerConnection = new IORedis(process.env.REDIS_URL, {
+    maxRetriesPerRequest: null
+});
 
-//The queue-jobs are added
-const executionQueue = new Queue("code-execution",{
+const executionQueue = new Queue("code-execution", {
     connection: queueConnection,
-    defaultJobOptions:{
-        attempts:1,  //no retries - code execution is -not idempotent(repetition changes the outcome)
-        removeOnComplete:50, //keep last 50 completed jobs in Redis for Bull board
-        removeOnFail: 50, //Keep last 50 failed jobs
+    defaultJobOptions: {
+        attempts: 1,
+        removeOnComplete: 50,
+        removeOnFail: 50,
     },
 });
 
-module.exports = { executionQueue, workerConnection, redisConfig};
+module.exports = { executionQueue, workerConnection };
